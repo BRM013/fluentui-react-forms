221e7eb4cdd772c39a077821c9d729d9b16b2159
Brett Morris <brm013@outlook.com>
2020-05-15 15:23:19 +1000

    Updates

---
+++ precache-manifest.8328f5c11149f2d43a3c5541b4b97562.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa96d64cf625f07fc9091113e07a257e",
+    "url": "/@brm013/fluentui-react-forms/index.html"
+  },
+  {
+    "revision": "b1dfb48cac85e59f619e",
+    "url": "/@brm013/fluentui-react-forms/static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "387e9e57adf5100cf7b0",
+    "url": "/@brm013/fluentui-react-forms/static/js/2.0b64a40f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/@brm013/fluentui-react-forms/static/js/2.0b64a40f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1dfb48cac85e59f619e",
+    "url": "/@brm013/fluentui-react-forms/static/js/main.d3835e99.chunk.js"
+  },
+  {
+    "revision": "ecfc678b188c81e4d664",
+    "url": "/@brm013/fluentui-react-forms/static/js/runtime-main.d6a11cf4.js"
+  }
+]);
